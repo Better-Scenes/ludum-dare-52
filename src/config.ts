930fc60348b102a563961d0725130c3cad4a5808
Russell Long <russelllongj@gmail.com>
2023-01-07 10:52:08 +1100

    Basic springy chain

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,23 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
 export default {
   type: Phaser.AUTO,
-  parent: 'game',
-  backgroundColor: '#33A5E7',
+  parent: "game",
+  backgroundColor: "#33A5E7",
   scale: {
     width: 800,
     height: 600,
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
+    autoCenter: Phaser.Scale.CENTER_BOTH,
   },
   physics: {
-    default: 'matter',
-    arcade: {
-        gravity: { y: 0 },
-        debug: {
-          showBody:true,
-          showStaticBody: true
-        }
-    }
-},
+    default: "matter",
+    matter: {
+      gravity: { y: 0 },
+      debug: {
+        showBody: true,
+        showStaticBody: true,
+      },
+    },
+  },
 };
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -6,12 +6,33 @@ export default class Demo extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('cranberry', 'assets/cranberry.png');
+    this.load.image("cranberry", "assets/cranberry.png");
   }
 
   create() {
-    const cranberry = this.add.image(400, 70, 'cranberry');
+    // const cranberry = this.add.image(400, 70, "cranberry");
 
+    const block = this.matter.add.image(400, 50, 'cranberry', 0, { ignoreGravity: true });
+    block.setFixedRotation();
+    block.setMass(500);
 
+    let y = 150;
+    let prev = block;
+
+    for (let i = 0; i < 12; i++) {
+        const ball = this.matter.add.image(400, y, 'cranberry', 0, { shape: 'circle', mass: 0.1, scale: { x: 1, y: 3 } });
+        // ball.scaleY = 3;
+        // ball.refreshBody();
+
+        this.matter.add.joint(prev, ball, (i === 0) ? 90 : 15, 0.4);
+
+        prev = ball;
+
+        y += 18;
+    }
+
+    this.matter.add.mouseSpring();
+
+    const cursors = this.input.keyboard.createCursorKeys();    
   }
 }
