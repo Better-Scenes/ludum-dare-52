454b676f4e8f25c7cd85f35abf8b37e262b95222
Russell Long <russelllongj@gmail.com>
2023-01-07 11:12:43 +1100

    Get the rope segments working

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -7,28 +7,28 @@ export default class Demo extends Phaser.Scene {
 
   preload() {
     this.load.image("cranberry", "assets/cranberry.png");
+    this.load.image("paddleSegment", "assets/paddle-segment.png");
   }
 
   create() {
     // const cranberry = this.add.image(400, 70, "cranberry");
 
-    const block = this.matter.add.image(400, 50, 'cranberry', 0, { ignoreGravity: true });
+    const block = this.matter.add.image(400, 50, 'paddleSegment', 0, { ignoreGravity: true });
     block.setFixedRotation();
-    block.setMass(500);
+    block.setMass(50000);
 
-    let y = 150;
+    let x = 150;
     let prev = block;
 
+    const segmentLength = 50;
     for (let i = 0; i < 12; i++) {
-        const ball = this.matter.add.image(400, y, 'cranberry', 0, { shape: 'circle', mass: 0.1, scale: { x: 1, y: 3 } });
-        // ball.scaleY = 3;
-        // ball.refreshBody();
+        const ball = this.matter.add.image(x, 50, 'paddleSegment', 0, {mass: 0.1, scale: { x: 1, y: 1 } });
 
-        this.matter.add.joint(prev, ball, (i === 0) ? 90 : 15, 0.4);
+        this.matter.add.joint(prev, ball, 5, 0.4, { pointA: { x: segmentLength * 0.5, y: 0 }, pointB: { x: -segmentLength * 0.5, y: 0 } });
 
         prev = ball;
 
-        y += 18;
+        x += segmentLength * 2.5;
     }
 
     this.matter.add.mouseSpring();
