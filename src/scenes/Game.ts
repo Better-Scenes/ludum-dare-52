6e108a215b835bef5c27e9b08111050cf84ce264
Rohan Richards <scenes@quartermoondesigns.com>
2023-01-07 12:09:16 +1000

    adds clickable bushes to spawn berries

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,10 +1,11 @@
 import Phaser from "phaser";
 
-import config from "../config"
+import config from "../config";
 
 enum assets {
   CRANBERRY = "cranberry",
-  PADDLESEGMENT = 'paddleSegment'
+  BUSH = "bush",
+  PADDLESEGMENT = "paddleSegment",
 }
 
 export default class Demo extends Phaser.Scene {
@@ -14,22 +15,28 @@ export default class Demo extends Phaser.Scene {
 
   preload() {
     this.load.image(assets.CRANBERRY, "assets/cranberry.png");
+    this.load.image(assets.BUSH, "assets/bush.png");
     this.load.image(assets.PADDLESEGMENT, "assets/paddle-segment.png");
   }
 
   create() {
     this.createFloat(100, 100);
-    
-    this.createBerries();
+    this.createBushes(10);
     this.matter.add.mouseSpring();
 
-    const cursors = this.input.keyboard.createCursorKeys();    
+    const cursors = this.input.keyboard.createCursorKeys();
   }
 
-  createFloat(startX, startY) {
+  createFloat(startX: number, startY: number) {
     const segmentLength = 50;
-    const numberOfSegments = 10
-    const startSegment = this.matter.add.image(startX, startY, assets.PADDLESEGMENT, 0, { ignoreGravity: true });
+    const numberOfSegments = 10;
+    const startSegment = this.matter.add.image(
+      startX,
+      startY,
+      assets.PADDLESEGMENT,
+      0,
+      { ignoreGravity: true }
+    );
     const jointSize = 5;
     startSegment.setFixedRotation();
     startSegment.setMass(50000);
@@ -39,28 +46,61 @@ export default class Demo extends Phaser.Scene {
     let prev = startSegment;
 
     for (let i = 0; i < numberOfSegments; i++) {
-        const segment = this.matter.add.image(startX, startY, assets.PADDLESEGMENT, 0, {mass: 0.1, scale: { x: 1, y: 1 }, frictionAir: 0.08 });
-        this.matter.add.joint(prev, segment, jointSize, 0.4, { pointA: { x: segmentLength * 0.5, y: 0 }, pointB: { x: -segmentLength * 0.5, y: 0 } });
-        segment.setMass(100);
+      const segment = this.matter.add.image(
+        startX,
+        startY,
+        assets.PADDLESEGMENT,
+        0,
+        { mass: 0.1, scale: { x: 1, y: 1 }, frictionAir: 0.08 }
+      );
+      this.matter.add.joint(prev, segment, jointSize, 0.4, {
+        pointA: { x: segmentLength * 0.5, y: 0 },
+        pointB: { x: -segmentLength * 0.5, y: 0 },
+      });
+      segment.setMass(100);
 
-        prev = segment;
+      prev = segment;
 
-        startX += segmentLength + jointSize;
+      startX += segmentLength + jointSize;
     }
   }
 
-  createBerries() {
-    const count = 100;
-    let startX = 10;
-    let startY = 10;
+  createBerries(count: number, startX: number, startY: number, range = 10) {
     for (let i = 0; i < count; i++) {
-      const berry = this.matter.add.image(getRandomInt(0, config.scale?.width ?? 500), getRandomInt(0, config.scale?.height ?? 500), assets.CRANBERRY, 0, {mass: 0.1, scale: { x: 1, y: 1 }, frictionAir: 0.04 });
-      startX +=10;
-      startY += 10;
+      const berry = this.matter.add.image(
+        getRandomInt(startX, startX + range),
+        getRandomInt(startY, startY + range),
+        assets.CRANBERRY,
+        0,
+        { mass: 0.1, scale: { x: 1, y: 1 }, frictionAir: 0.04 }
+      );
+    }
+  }
+
+  createBushes(count: number) {
+    for (let i = 0; i < count; i++) {
+      const bush = this.matter.add.image(
+        getRandomInt(0, config.scale?.width ?? 500),
+        getRandomInt(0, config.scale?.height ?? 500),
+        assets.BUSH,
+        0,
+        {
+          mass: 0.1,
+          scale: { x: 1, y: 1 },
+          frictionAir: 1,
+          isSensor: true,
+          isStatic: true,
+        }
+      );
+      bush.setInteractive();
+      bush.on("pointerdown", () => {
+        console.log("clicked bush");
+        this.createBerries(getRandomInt(5, 10), bush.x, bush.y);
+        bush.destroy();
+      });
     }
   }
 }
-
 
 /**
  * Returns a random integer between min (inclusive) and max (inclusive).
@@ -69,7 +109,7 @@ export default class Demo extends Phaser.Scene {
  * lower than max if max isn't an integer).
  * Using Math.round() will give you a non-uniform distribution!
  */
-function getRandomInt(min, max) {
+function getRandomInt(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
