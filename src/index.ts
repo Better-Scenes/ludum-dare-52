8abf9b97c9b7e9648a13374efddf6882b62bc541
Rohan Richards <scenes@quartermoondesigns.com>
2023-01-09 09:53:08 +1000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,10 @@ new Phaser.Game(
   Object.assign(config, {
     // scene: [GameScene, MenuScene, GameOver],
     scene: [MenuScene, GameScene, GameOver],
+    callbacks: {
+      preBoot: (game) => {
+        game.music = Phaser.Sound.SoundManagerCreator.create(game);
+      },
+    },
   })
 );
